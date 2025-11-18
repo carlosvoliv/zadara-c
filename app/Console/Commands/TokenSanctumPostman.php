d6a49c054d43d9c8123bad381457a919fb4ea721
carlosvoliv <carlos.oliveira@tecksolucoes.com.br>
2025-11-18 15:21:44 -0300

    tokenSanctum

---
+++ app/Console/Commands/TokenSanctumPostman.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TokenSanctumPostman extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:token-sanctum-postman';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
+
