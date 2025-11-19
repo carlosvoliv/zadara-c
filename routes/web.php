29ce058c9ab623114a14eb2ecf4ba895619c6370
carlosvoliv <carlos.oliveira@tecksolucoes.com.br>
2025-11-19 10:50:50 -0300

    Preparing zip test

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Api\ZadaraController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('upload-zip', 'upload-zip');
+Route::post('api/zadara/upload-zip', [ZadaraController::class, 'uploadZip']);
 Route::get('api/zadara/lista', [ZadaraController::class, 'lista']);
 Route::get('api/zadara/download/{path}', [ZadaraController::class, 'download'])
      ->where('path', '.*');
